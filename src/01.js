80e96b92614a44b1f4c8fc71ac08584e56751ded
yangzhou <yearrow@qq.com>
2023-09-17 21:05:33 +0800

    ast parser

---
+++ src/01.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const parser = require("@babel/parser");
+const traverse = require('@babel/traverse').default;
+const t = require('@babel/types');
+const generator = require('@babel/generator').default;
+
+const path = require('path');
+
+// const jsCode = fs.readdirSync('./demo.js',{encoding:'utf-8'});
+const jsCode = `let a= 100;\n // 测试函数\n function fn(a,b){console.log(a,b)}`
+const ast = parser.parse(jsCode);
+
+let visitor = {
+    FunctionExpression:function (path){
+        console.log(path);
+        console.log('hello');
+    },
+
+    enter(path){
+        console.log(path);
+    },
+    exit(path){
+
+        console.log(path);
+    }
+}
+
+// 遍历ast
+traverse(ast, visitor)
+
+// 代码生成
+let code = generator(ast, {
+    retainLines: false,
+    comments: true,
+    compact: false,
+}).code
+console.log(code);
+
+fs.writeFile('./newDemo.js', code, (err) => {
+
+})
---
+++ src/newDemo.js
@@ -0,0 +1,5 @@
+let a = 100;
+// 测试函数
+function fn(a, b) {
+  console.log(a, b);
+}
